a597d847e5475d8fc44cf4322881248a5218215a
Clemens <clemensmollik@gmail.com>
2020-03-06 13:40:10 +0100

    Add 3DLevel and create example Measurements

---
+++ demo_unity/DemoVR/Assets/Scripts/3DLevel/Measurement3DLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Measurement3DLevel : Measurement
+{
+    public string scaling;
+    public List<int> buttonPositionLabel;
+    public List<float> buttonPosition;
+
+    public Measurement3DLevel(List<int> buttonPositionLabel, List<float> buttonPosition, Scaling scaling, string level, TriggerState currentState) : base(level, currentState)
+    {
+        this.buttonPositionLabel = buttonPositionLabel;
+        this.buttonPosition = buttonPosition;
+        this.scaling = scaling.ToString();
+    }
+}
