6d389ff5fabab66cb7c49866620dddbdd60700e6
VR_Gaze <it16127@lehre.dhbw-stuttgart.de>
2020-05-23 11:38:44 +0200

    refactored and comment button classes

--- demo_unity/DemoVR/Assets/Scripts/ButtonCanvas.cs
+++ demo_unity/DemoVR/Assets/Scripts/ButtonCanvas.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// class for changing the canvas of the button
+/// </summary>
 public class ButtonCanvas : BaseMono
 {
     private const float scalingX = 0.005f;
     private const float scalingY = 0.01f;
     private const float scalingZ = 0.02f;
 
+    /// <summary>
+    /// scaling property changed event handler
+    /// call setScale for setting the new scale
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ScalingProperty_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
     {
         this.setScale(scalingX * ScalingProperty.scaleWidth, scalingY * ScalingProperty.scaleHeight, scalingZ);
     }
 
+    /// <summary>
+    /// sets the scale by the given values
+    /// </summary>
+    /// <param name="x">scale on x axis</param>
+    /// <param name="y">scale on y axis</param>
+    /// <param name="z">scale on z axis</param>
     private void setScale(float x, float y, float z)
     {
         Vector3 localScale = this.transform.localScale;
@@ -22,6 +37,9 @@ public class ButtonCanvas : BaseMono
         this.transform.localScale = localScale;
     }
 
+    /// <summary>
+    /// resets the position of the button in y axis
+    /// </summary>
     private void resetPosition()
     {
         Vector3 pos = this.transform.localPosition;
@@ -29,6 +47,13 @@ public class ButtonCanvas : BaseMono
         this.transform.localPosition = pos;
     }
 
+    /// <summary>
+    /// Performs while starting following tasks:
+    /// 
+    /// resets the position
+    /// set the scaling to the const values
+    /// register scaling property changed event handler
+    /// </summary>
     protected override void DoStart()
     {
         this.resetPosition();
@@ -36,22 +61,34 @@ public class ButtonCanvas : BaseMono
         ScalingProperty.PropertyChanged += ScalingProperty_PropertyChanged;
     }
 
+    /// <summary>
+    /// nothing to do when object will be awaked
+    /// </summary>
     protected override void DoAwake()
     {
         // nothing todo
     }
 
+    /// <summary>
+    /// unregister scaling property changed event handler and resets position before destroying the object
+    /// </summary>
     protected override void DoDestroy()
     {
         ScalingProperty.PropertyChanged -= ScalingProperty_PropertyChanged;
         this.resetPosition();
     }
 
+    /// <summary>
+    /// nothing to do when eyetracking calibration started
+    /// </summary>
     protected override void OnCalibrationStarted()
     {
         // nothing todo
     }
 
+    /// <summary>
+    /// nothing to do when eyetracking calibration routine is done
+    /// </summary>
     protected override void OnCalibrationRoutineDone()
     {
         // nothing todo
--- demo_unity/DemoVR/Assets/Scripts/ButtonNumber.cs
+++ demo_unity/DemoVR/Assets/Scripts/ButtonNumber.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// class for the buttons with numbers
+/// </summary>
 public class ButtonNumber: GeneralButton
 {
     public BaseGame game;
@@ -10,44 +13,53 @@ public class ButtonNumber: GeneralButton
     private bool isCalibrationRunning = false;
     private Color buttonColor = Color.red;
 
+    /// <summary>
+    /// gets the game object after loading
+    /// </summary>
     protected override void DoStart()
     {
         base.DoStart();
         GameObject go = GameObject.FindGameObjectWithTag("Game");
         game = go.GetComponent<BaseGame>();
-        // GameObject go = new GameObject();
-        // CylinderCollider cc = this.gameObject.AddComponent<CylinderCollider>();
-        // cc.radius = 65;
-        // cc.height = 1;
-        // cc.boxCount = 5;
-        // cc.widthScale = 1.6f;
-        // go.transform.localScale = this.transform.localScale;
-        // go.transform.position = this.transform.position;
-        // //go.transform.parent = this.transform;
     }
 
+    /// <summary>
+    /// set flag isCalibrationRunning to true while eyetracking calibration is running
+    /// </summary>
     protected override void OnCalibrationStarted()
     {
         this.isCalibrationRunning = true;
     }
 
+    /// <summary>
+    /// set flag isCalibrationRunning to false when eyetracking calibration is stopped
+    /// </summary>
     protected override void OnCalibrationRoutineDone()
     {
         this.isCalibrationRunning = false;
     }
 
+    /// <summary>
+    /// Sets the text of the Button to the defined number
+    /// </summary>
     protected override void DoAwake()
     {
         Text text = this.GetComponentInChildren<Text>();
         text.text = this.number.ToString();
     }
 
+    /// <summary>
+    /// nothing to do when object will be destroyed
+    /// </summary>
     protected override void DoDestroy()
     {
         // nothing todo
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Performs the action when button is clicked. 
+    /// Asks the game if this is the searched button, when yes than the color of the button will be set to green otherwise to red
+    /// </summary>
     public override void DoAction()
     {
         if (!this.isCalibrationRunning)
