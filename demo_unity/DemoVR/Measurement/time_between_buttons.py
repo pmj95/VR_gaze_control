b423e1c0d48e440ed8e733b48734ac8b26911b13
Clemens <clemensmollik@gmail.com>
2020-05-27 23:52:44 +0200

    analyzer.py

--- demo_unity/DemoVR/Measurement/time_between_buttons.py
+++ demo_unity/DemoVR/Measurement/time_between_buttons.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
 import math
 DIST_X = 2.5
@@ -12,8 +13,45 @@ def calc_time_between(data):
     for dicts in data:
         update_dicts(times, dicts)
     print(times)
+    avg_times = get_avrg_times(times)
+    plot_avg_times(avg_times)
     pass
 
+
+def get_avrg_times(times):
+    avrg_times = [[[], []], [[], []], [[], []], [[], []], [[], []], [[], []], [[], []], [[], []]]
+    for k in range(8):
+        for dist in times[k]:
+            if len(avrg_times[k][0]) == 0:
+                avrg_times[k][0].append(dist)
+                avrg_times[k][1].append((sum(times[k][dist]) / len(times[k][dist])) / 1000)
+            else:
+                i = 0
+                while avrg_times[k][0][i] < dist:
+                    i += 1
+                    if i == len(avrg_times[k][0]):
+                        break
+                avrg_times[k][0].insert(i, dist)
+                avrg_times[k][1].insert(i, (sum(times[k][dist])/len(times[k][dist]))/1000)
+    return avrg_times
+
+
+def plot_avg_times(times):
+    data_labels = ['Laser/Trigger', 'Laser/Blink', 'Eye/Trigger', 'Eye/Blink']
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(times[0][0], times[0][1])
+    axs[0, 0].plot(times[1][0], times[1][1])
+    axs[0, 0].set_title('Laser/Trigger')
+    axs[0, 1].plot(times[2][0], times[2][1])
+    axs[0, 1].plot(times[3][0], times[3][1])
+    axs[0, 1].set_title('Laser/Blink')
+    axs[1, 0].plot(times[4][0], times[4][1])
+    axs[1, 0].plot(times[5][0], times[5][1])
+    axs[1, 0].set_title('Eye/Trigger')
+    axs[1, 1].plot(times[6][0], times[6][1])
+    axs[1, 1].plot(times[7][0], times[7][1])
+    axs[1, 1].set_title('Eye/Blink')
+    plt.savefig("plot_buttons.png", format='png', dpi=200)
 
 def update_dicts(times, dicts):
     i = 0
@@ -56,10 +94,11 @@ def add_values(times_dict, file):
 def update_single_dict(previous, current, times_dict):
     time_diff = current[0] - previous[0]
     dist = get_distance_between_buttons(current[1], previous[1])
-    if time_diff in times_dict:
+    if dist in times_dict.keys():
         times_dict[dist].append(time_diff)
     else:
         times_dict[dist] = [time_diff]
+
 
 def get_distance_between_buttons(a, b):
     a = a - 1
