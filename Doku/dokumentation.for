3a1404f26ef6b98a1a7d496aff30e2aade3658c5
Clemens <clemensmollik@gmail.com>
2020-06-07 15:32:03 +0200

    updates

--- Doku/dokumentation.for
+++ Doku/dokumentation.for
@@ -7,13 +7,17 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\addvspace {10\p@ }
+\contentsline {formel}{\numberline {2.1} Index of Difficulty Berechnung}{17}{equation.2.5.1}%
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {2.2} Movement Time Berechnung}{17}{equation.2.5.2}%
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {5.1} Anzahl unterschiedlicher Versuchsaufbauten}{46}{equation.5.2.1}%
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
